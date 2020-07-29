a6b9fe03077d16a57381ddfdd0c9a45a18634617
unknown <vladimirvoron7@gmail.com>
2020-07-29 15:58:01 +0300

    parsing

---
+++ Bunker/Bunker/EnumController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bunker
+{
+    class EnumController
+    {
+    }
+}
---
+++ Parsing.py
@@ -0,0 +1,93 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+def getresponse():
+    response = requests.get(url)
+    part = response.text
+    part = part.replace(r'\nКарта №1',r';\nКарта №1')
+    part = part.replace('{"text":"','').replace(r'\n','').replace('"}','')
+    partlist = part.split(';')
+    partlist = partlist[0:-1]  #Приведение запроса к нужному виду
+    return partlist
+
+def main():
+    for i in range(1,1000):
+        partlist = getresponse()
+
+        for k,v in indexdict.items():
+            result = partlist[k].partition(':')[2]
+            result = result.strip()
+            result = result.capitalize()
+            if result not in globals()[v]:
+                globals()[v].append(result)
+        #print(f'{i}')
+
+    for k,v in indexdict.items():
+        with open(f'data\{v}.txt','w') as file:
+            for elem in globals()[v]:
+                file.write(elem)
+                file.write('\n')
+
+    with open(f'data\gender.txt','w') as file:
+            for elem in gender:
+                file.write(elem)
+                file.write('\n')
+
+    with open(f'data\old.txt','w') as file:
+            for elem in old:
+                file.write(str(elem))
+                file.write('\n')
+
+    for k,v in indexdict.items():
+        print(len(globals()[v]))
+        
+url = 'https://randomall.ru/api/custom/gen/758/'
+
+gender = ['Мужской','Женский']
+old = [x for x in range(1,90)]
+prof = []
+kids = []
+health = []
+phobias = []
+character = []
+hobby = []
+additionally = []
+baggage = []
+card1 = []
+card2 = []
+
+indexdict = {2:'prof',
+3:'kids',
+4:'health',
+5:'phobias',
+6:'hobby',
+7:'character',
+8:'additionally',
+9:'baggage',
+10:'card1',
+11:'card2'}
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
